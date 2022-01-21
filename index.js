fdd6bc6647e6a505448d81f9e0c4c2d140783848
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-01-20 18:54:31 -0600

    Agregando el mongoose y el modelo de Cliente

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 
+//Conectar mongo
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/restapis', {
+    useNewUrlParser: true
+});
 
 //crear el servidor
 const app = express();
