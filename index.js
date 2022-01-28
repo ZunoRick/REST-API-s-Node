aa18a1f6a5320a01c5197298417beaca2040e91a
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-01-27 18:20:18 -0600

    Preparando archivos para deployment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env'});
 
 /*Cors permite que un cliente se conecte a otro 
 servidor para el intercambio de recursos*/
@@ -9,7 +10,7 @@ const path = require('path');
 
 //Conectar mongo
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/restapis', {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true
 });
 
@@ -20,6 +21,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+//Definir un dominio o dominios para recibir las peticiones
+const whiteList = [process.env.FRONTEND_URL];
+const corsOptions = {
+    origin: (origin, callback) =>{
+        //Revisar si la petición viene de un servidor que está en la whiteList
+        const existe = whiteList.some( dominio => dominio === origin);
+        if (existe) {
+            callback(null, true);
+        }else{
+            callback(new Error('No permitido por CORS'));
+        }
+    }
+}
+
 //Habilitar cors
 app.use(cors());
 
@@ -29,5 +44,10 @@ app.use('/', routes());
 //carpeta publica
 app.use("/uploads", express.static(path.resolve(__dirname, 'uploads')));
 
-//puerto
-app.listen(5000);
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 5000;
+
+//iniciar app
+app.listen(port, host, () => {
+    console.log('El servidor está corriendo en ', port);
+});
